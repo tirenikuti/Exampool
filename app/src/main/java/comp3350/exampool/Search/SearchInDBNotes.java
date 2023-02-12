233f5829d5dd842b4fd3682f8fd3746f14ca0d5f
GIRISH JAGROOP <jagroopg@myumanitoba.ca>
2023-02-12 06:31:07 +0000

    Searching for notes in database with key

---
+++ app/src/main/java/comp3350/exampool/Search/SearchInDBNotes.java
@@ -0,0 +1,45 @@
+// This class searches the contents of the Notes table in JDBC
+
+import java.sql.*;
+ 
+public class SearchInDBNotes {
+ 
+    // Main driver method
+    public static void main(String[] args)
+    {
+        //Establishing a connection
+        ConnectionDatabaseNotes connection = new ConnectionDatabaseNotes();
+
+        Connection connect = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        connect = connection.ConnectionDatabaseNotes();
+ 
+        // Try block to check exceptions
+        try {
+ 
+            //Write  and process the statement
+            String sql="select * from cuslogin where id="+Integer.parseInt(textfield.getText());
+            ps = connect.prepareStatement(sql);
+            rs = ps.executeQuery();
+ 
+            //Checking for next() till empty
+            if (rs.next()) {
+               
+                int id = rs.getInt("Id: ");
+                String userName = rs.getString("User Name: ");
+                String email = rs.getString("Email: ");
+                String password = rs.getString("Password: ");
+                 
+              // Print and display name, emailID and password
+              System.out.println(id + "\t\t" + userName + "\t\t" + email + "\t\t" + password);
+            }
+        }//try
+        catch (SQLException e) {
+ 
+            // Print the exception
+            System.out.println(e);
+        }//Catch
+    }//main
+}//SearchInDB
