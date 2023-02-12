25526ac7064f750f8f5249466fcde868d91ba695
BuildTools <unconfigured@null.spigotmc.org>
2023-02-12 13:55:48 -0600

    Added an AllTests test suite

---
+++ app/src/test/java/comp3350/exampool/AllTests.java
@@ -0,0 +1,14 @@
+package comp3350.exampool;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        FlashcardTest.class,
+        NotesTest.class,
+})
+public class AllTests {
+
+
+}
