2e8cddd92604bc0d1581876bb39e837b9fcefd03
GIRISH JAGROOP <jagroopg@myumanitoba.ca>
2023-02-12 08:49:18 +0000

    Tag search code updated

--- app/src/main/java/comp3350/exampool/Tags/Tags.java
+++ app/src/main/java/comp3350/exampool/Tags/Tags.java
@@ -1,20 +1,45 @@
 package comp3350.exampool.Tags;
 
 import java.util.ArrayList;
+import java.util.ArrayList; // import the ArrayList class
+
 
 //Placeholder class to fix errors until next iteration
 public class Tags {
-
-
-    public static ArrayList<Integer> searchTag(String target){
-        return null;
+    public Tags(){
     }
-
-    public static Tags tagAtIndex(ArrayList<Integer> i){
-        return null;
-    }
-
+    
     public String getTagName(){
         return null;
     }
+
+public ArrayList searchTag(String searchName){
+  ArrayList<Integer> result = new ArrayList<Integer>();
+  if (head != null) {  
+    Node temp = head;
+    int foundAtIndex = 0;
+    while (temp != null) {
+      //Store index of that particular element, if found.
+      if ((temp.getTagName()).equals(searchName)) {
+        result.add(index);//If tag was found, add it's particular index to the arraylist
+      }
+      //Gradually increases index while
+      //traversing through the Linked List
+      index++;
+      temp = temp.next;
+    }
+  }
+  return result;
+}
+
+public Tags tagAtIndex(int index){//Function to return a tag found at index 
+  Node temp = head;
+  if(index <= size()){  
+    for (int i = 0; i < index; i++) {
+      temp.next();
+    }
+  }
+  return temp;
+}
+
 }
