d7f9421aa5b23630cbe63571ddd44235e90981b5
GIRISH JAGROOP <jagroopg@myumanitoba.ca>
2023-02-12 20:36:29 +0000

    Error fixed

--- app/src/main/java/comp3350/exampool/Tags/Tags.java
+++ app/src/main/java/comp3350/exampool/Tags/Tags.java
@@ -3,11 +3,9 @@ package comp3350.exampool.Tags;
 //Placeholder class to fix errors until next iteration
 public class Tags {
     private String tagName;//The name of the tag
-    private static noOfTags =0;//The number of tags that exists
 
     public Tags(String tN) {
         tagName = tN;
-        noOfTags++;
     }
 
     public String getTagName() {
