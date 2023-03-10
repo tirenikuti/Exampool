6d5c5dd8967a20896a8814cf1d54e139e0c8a2bb
Krupali Prajapati <prajapak@myumanitoba.ca>
2023-03-10 18:44:59 +0000

    Update UserPersistence.java

--- app/src/main/java/comp3350/exampool/persistence/UserPersistence.java
+++ app/src/main/java/comp3350/exampool/persistence/UserPersistence.java
@@ -2,6 +2,14 @@ package comp3350.exampool.persistence;
 
 import java.util.List;
 
+import comp3350.exampool.objects.User;
+
 public interface UserPersistence {
-    
+    List<User> getUserSequential();
+
+    List<User> getUser(final User currentUser);
+
+    User insertUser(final User currentUser);
+
+    void deleteUser(final User currentUser);
 }
