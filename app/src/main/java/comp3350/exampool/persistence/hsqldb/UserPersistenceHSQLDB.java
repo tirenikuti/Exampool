45abf3c516694c159b136e1889413ad31bd9aca3
Krupali Prajapati <prajapak@myumanitoba.ca>
2023-03-10 19:05:40 +0000

    Update UserPersistenceHSQLDB.java

--- app/src/main/java/comp3350/exampool/persistence/hsqldb/UserPersistenceHSQLDB.java
+++ app/src/main/java/comp3350/exampool/persistence/hsqldb/UserPersistenceHSQLDB.java
@@ -23,10 +23,102 @@ public class UserPersistenceHSQLDB implements UserPersistence {
         return DriverManager.getConnection("jdbc:hsqldb:file" + dbPath + "; shutdown=true", "User", "");
     }
 
-    private void fromResultSet(final ResultSet rs) throws SQLException {
+    private User fromResultSet(final ResultSet rs) throws SQLException {
         final String userID = rs.getString("userID");
         final String userName = rs.getString("name");
         final String type = rs.getString("accountType");
         
+        return new User(userID, type, userName);
+    }
+
+    @Override
+    public List<User> getUserSequential() {
+        final List<User> users = new ArrayList<>();
+        try (final Connection c = connection()){
+            final Statement st = c.createStatement();
+            final ResultSet rs = st.executeQuery("Select * from users");
+            while (rs.next()) {
+                final User user = fromResultSet(rs);
+                users.add(user);
+            }
+            rs.close();
+            st.close();
+
+            return users;
+        } catch (final SQLException e){
+            throw new PersistenceException(e);
+        }
+    }
+
+    @Override
+    public List<User> getUser(User currentUser) {
+        final List<User> users = new ArrayList<>();
+        try (final Connection c = connection()) {
+            final PreparedStatement st = c.prepareStatement("Select * FROM users WHERE userID = ?");
+            st.setString(1, currentUser.getUserID);
+
+            final ResultSet rs = st.executeQuery();
+            while(rs.next()) {
+                final User user = fromResultSet(rs);
+                users.add(user);
+            }
+            rs.close();
+            st.close();
+
+            return users;
+        } catch (final SQLException e){
+            throw new PersistenceException(e);
+        }
+    }
+
+    @Override
+    public User insertUser(User currentUser){
+        try (final Connection c = connection()) {
+            final PreparedStatement st = c.prepareStatement("INSERT INTO users VALUES(?, ?, ?)");
+            st.setString(1, currentUser.getUserID());
+            st.setString(2, currentUser.getAccountType());
+            st.setString(3, currentUser.getUserName());
+            st.executeUpdate();
+
+            return currentUser;
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
+    }
+
+    @Override
+    public User updateUser(User currentUser) {
+        try (final Connection c = connection()) {
+            final PreparedStatement st = c.prepareStatement("UPDATE users SET accountType = ?, userName = ? WHERE userID = ?)");
+            st.setString(1, currentUser.getAccountType());
+            st.setString(2, currentUser.getUserName());
+            st.setString(3, currentUser.getUserID());
+            st.executeUpdate();
+
+            return currentUser;
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
+    }
+
+    @Override
+    public void deleteUser(User currentUser) {
+        try (final Connection c = connection()) {
+            final PreparedStatement sc = c.prepareStatement("DELETE FROM flashcards WHERE userID = ?");
+            sc.setString(1, currentUser.getUserID());
+            sc.executeUpdate();
+
+            final PreparedStatement st = c.prepareStatement("DELETE FROM notes WHERE userID = ?");
+            st.setString(1, currentUser.getUserID());
+            st.executeUpdate();
+
+            final PreparedStatement st = c.prepareStatement("DELETE FROM users WHERE userID = ?");
+            st.setString(1, currentUser.getUserID());
+            st.executeUpdate();
+            
+        } catch (final SQLException e) {
+            throw new PersistenceException(e);
+        }
+
     }
 }
