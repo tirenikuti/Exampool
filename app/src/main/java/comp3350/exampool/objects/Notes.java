21515f26e0a271c89c348e7b50ff840c46972cbf
Christopher Wasowicz <wasowicc@myumanitoba.ca>
2023-03-22 12:06:03 -0500

    Updated Notes comments and removed all association with tags as well as removed tag realated tests in NotesTest

--- app/src/main/java/comp3350/exampool/objects/Notes.java
+++ app/src/main/java/comp3350/exampool/objects/Notes.java
@@ -1,14 +1,11 @@
 package comp3350.exampool.objects;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 /**
- * Summary: NoteActivity holds a string of content
- * Parameters: 2 Constructors one with tags one without
+ * Summary: Notes holds a string of content
+ * Parameters: 2 Constructors one with only noteID and one with all parameters defined
  * Returns: Getters for all class variables
- * Description: NoteActivity has some string of content, ID number for the user who made it and the note itself
- * as well as tags that describe the category of the notes.
+ * Description: Notes has some Strings for content, ID of the user who made it and ID of the note itself
  */
 
 public class Notes{
@@ -16,52 +13,36 @@ public class Notes{
     private String noteID;
     private String userID;
     private String content;
-    private final ArrayList<String> tags;
 
     /**
-     * Constructor with just notesID - for AccessNotes
-     * @param notesID
+     * Constructor: Create a new note with only a noteID, used for AccessNotes
+     * @param notesID noteID Type: String
      */
     public Notes(String notesID){
         noteID = notesID;
         userID = null;
         content = null;
-        tags = null;
     }
     /**
-     * Constructor: Create a new note without tags
-     * @param uID UserID Type: String
-     * @param nID NoteID Type: String
-     * @param text Content Type: String
+     * Constructor: Create a new note with all parameters defined
+     * @param uID userID Type: String
+     * @param nID noteID Type: String
+     * @param text content Type: String
      */
     public Notes(String nID, String uID, String text){
         noteID = nID;
         userID = uID;
         content = text;
-        tags = null;
     }
 
-    /**
-     * Constructor: Create a new note with tags
-     * @param uID CreatorID Type: int
-     * @param nID NoteID Type: int
-     * @param text Content Type: String
-     * @param tagList List of categories for the notes Type: ArrayList <String>
-     */
-    public Notes(String nID, String uID, String text, ArrayList<String> tagList){
-        noteID = nID;
-        userID = uID;
-        tags = new ArrayList<>(tagList); //tags already exist
-        content = text;
-    }
 
     //In the App, getNote would display the note in an editable text box and editNote would update
     //the note once the App says it has been "saved"
 
     /**
      * Setter for note Content
-     * Replace content of the note with new text
-     * @param newText Text to replace content with Type: String
+     * Replace the contents of the note with new text
+     * @param newText Text to replace content with, Type: String
      */
     public void editNote(String newText){
         content = newText;
@@ -77,7 +58,7 @@ public class Notes{
 
     /**
      * Getter for the note's ID number
-     * @return noteID Type: Int
+     * @return noteID Type: String
      */
     public String getNoteID(){
         return noteID;
@@ -85,44 +66,10 @@ public class Notes{
 
     /**
      * Getter for the UserID
-     * @return UserID Type: String
+     * @return userID Type: String
      */
     public String getUserID(){
         return userID;
     }
 
-    /**
-     * For adding a new tag to the note
-     * @param newTag Type: String
-     * @return Tag was added? Type: Boolean
-     */
-    public boolean addTag(String newTag){
-        if(!newTag.equals("")){
-            tags.add(newTag);
-            return true; //tag was added
-        }
-        return false; //tag was not added
-    }
-
-    /**
-     * Removes tag from list of tags
-     * @param target Tag to remove Type: String
-     * @return Tag was removed? Type: Boolean
-     */
-    public boolean removeTag(String target){
-        if(tags.contains(target)){
-            tags.remove(target);
-            return true;//tag removed successfully
-        }
-        return false;//no tag to remove
-    }
-
-
-    /**
-     * Getter returns the tags for this note as an ArrayList
-     * @return tags Type: ArrayList <String>
-     */
-    public Collection<String> getTags() {
-        return tags;
-    }
 }
--- app/src/test/java/comp3350/exampool/NotesTest.java
+++ app/src/test/java/comp3350/exampool/NotesTest.java
@@ -1,75 +1,41 @@
 package comp3350.exampool;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 import comp3350.exampool.objects.Notes;
 
 public class NotesTest {
 
-    private Notes noteWithTags;
-    private Notes noteNoTags;
-    private final ArrayList<String> noteTags = new ArrayList<>();
+    private Notes note;
+    private Notes note2;
 
-    private final static String CONTENT_W_TAGS = "Hello World";
-    private final static String CONTENT_NO_TAGS = "Buy Bread";
+    private final static String CONTENT_1 = "Hello World";
+    private final static String CONTENT_2 = "Buy Bread";
     private final static String U_ID = "1";
-    private final static String N_ID_WITH = "1";
-    private final static String N_ID_NO = "2";
+    private final static String N_ID_1 = "1";
+    private final static String N_ID_2 = "2";
     @Before
     public void CreateNotes(){
-        noteTags.add("Math");
-        noteTags.add("Comp");
-        noteNoTags = new Notes(U_ID, N_ID_NO, CONTENT_NO_TAGS);
-    }
-
-    @After
-    public void cleanUp(){
-        noteTags.clear();
-    }
-
-    @Test
-    public void getTagsTest(){
-        Collection<String> tags, noTags;
-        noTags = noteNoTags.getTags();
-        tags = noteWithTags.getTags();
-        assertEquals("should be the same Collection of Tags", noteTags, tags);
-        assertTrue("The Collection of Tags should be empty", noTags.isEmpty());
-    }
-
-    @Test
-    public void addTagsTest(){
-        assertFalse("fails to add empty tag", noteNoTags.addTag(""));
-        assertTrue("succeeds adding a new tag", noteWithTags.addTag("Test1"));
-    }
-
-    @Test
-    public void removeTagsTest(){
-        assertFalse("fails to delete tag from empty list", noteNoTags.removeTag("test"));
-        assertTrue("succeeds deleting a tag", noteWithTags.removeTag("Math"));
-        assertFalse("fails to delete tag not in the list", noteWithTags.removeTag("test"));
+        note = new Notes(U_ID, N_ID_1, CONTENT_1);
+        note2 = new Notes(U_ID, N_ID_2, CONTENT_2);
     }
 
     @Test
     public void getEditTest(){
-        assertEquals("Note should be unchanged", CONTENT_W_TAGS, noteWithTags.getNote());
+        assertEquals("Note should be unchanged", CONTENT_1, note.getNote());
         String newText = "I've been replaced";
-        noteWithTags.editNote(newText);
-        assertEquals("Note should contain newText", newText, noteWithTags.getNote());
+        note.editNote(newText);
+        assertEquals("Note should contain newText", newText, note.getNote());
     }
 
     @Test
     public void getIDTest(){
-        assertEquals("Note should have User ID: 1", U_ID, noteNoTags.getUserID());
-        assertEquals("Note should have Note ID: 2", N_ID_NO, noteNoTags.getNoteID());
+        assertEquals("Note should have User ID: 1", U_ID, note2.getUserID());
+        assertEquals("Note should have Note ID: 2", N_ID_2, note2.getNoteID());
     }
 
 }
