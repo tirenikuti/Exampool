0597e05813d56f34de4d88751c12a969a2a906c3
krupali2000 <krupsp2000@gmail.com>
2023-04-08 20:05:12 -0500

    Testing start

---
+++ app/src/test/java/comp3350/exampool/business/AccessNotesIT.java
@@ -0,0 +1,22 @@
+package comp3350.exampool.business;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import comp3350.exampool.application.Main;
+import comp3350.exampool.business.AccessNotes;
+import comp3350.exampool.objects.Notes;
+import comp3350.exampool.persistence.NotesPersistence;
+import comp3350.exampool.persistence.hsqldb.NotesPersistenceHSQLDB;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class AccessNotesIT {
+}
