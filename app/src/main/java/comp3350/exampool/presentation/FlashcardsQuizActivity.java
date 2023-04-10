2cac7d2d4c6c140ad29181853a2f557fbe47f206
Ryley Livingston <living17@myumanitoba.ca>
2023-04-10 14:26:38 -0500

    Flipping animation for quiz that doesn't show answer - Fixed

--- app/src/main/java/comp3350/exampool/presentation/FlashcardsQuizActivity.java
+++ app/src/main/java/comp3350/exampool/presentation/FlashcardsQuizActivity.java
@@ -2,6 +2,7 @@ package comp3350.exampool.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 
@@ -84,23 +85,6 @@ public class FlashcardsQuizActivity extends AppCompatActivity {
     }
 
 
-    public void flip(Boolean flipToFront){
-        TextView front = findViewById(R.id.typed_question);
-        TextView back = findViewById(R.id.revealed_answer);
-            if(flipToFront) {
-                front_animation.setTarget(front);
-                back_animation.setTarget(back);
-                front_animation.start();
-                back_animation.start();
-                isFront = false;
-            } else {
-                front_animation.setTarget(back);
-                back_animation.setTarget(front);
-                back_animation.start();
-                front_animation.start();
-                isFront = true;
-            }
-        }
     public void resetAllFlashcards(){
         for(int i = 0; i < flashcardList.size(); i++){
             Flashcard flashcard = flashcardList.get(i);
@@ -111,11 +95,29 @@ public class FlashcardsQuizActivity extends AppCompatActivity {
     }
 
     public void revealAnswer() {
-        isFront = true;
         Flashcard flashcard = flashcardList.get(position);
         TextView questionView = (TextView) findViewById(R.id.typed_question);
         questionView.setText(flashcard.getAnswer());
         flashcard.answered();
+    }
+    public void resetQuestion() {
+        TextView front = findViewById(R.id.typed_question);
+        TextView back = findViewById(R.id.revealed_answer);
+        back.setVisibility(View.INVISIBLE);
+
+        back_animation.setTarget(front);
+        back_animation.start();
+
+
+        //Delay 1 second so animation can run
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                back.setVisibility(View.VISIBLE);
+            }
+        }, 1000); // 1 second
+
+
+        isFront = true;
     }
 
     public void buttonNextOnClick(View view) {
@@ -123,16 +125,19 @@ public class FlashcardsQuizActivity extends AppCompatActivity {
         Flashcard flashcard = flashcardList.get(position);
         String submission = answerEdit.getText().toString();
         scored(flashcard, submission);
-        flip(isFront);
+
         if(position < flashcardList.size() - 1){
             position++;
             flashcard = flashcardList.get(position);
             TextView questionView = (TextView) findViewById(R.id.typed_question);
             TextView answer = (TextView) findViewById(R.id.editInputAnswer);
             TextView answerView = (TextView) findViewById(R.id.revealed_answer);
+            resetQuestion();
+
+
 
             questionView.setText(flashcard.getQuestion() + flashcard.getOptions());
-
+            answerView.setText(flashcard.getAnswer());
             answer.setText("");
             Button submitButton = findViewById(R.id.submitButton);
             submitButton.setEnabled(true);
